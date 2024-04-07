b902e314708d588c9ee5052b9e5c694ec0a6a18f
EVANA ANN BENNY <116190736+evansiehere@users.noreply.github.com>
2024-04-07 14:13:59 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,19 +1,19 @@
+/*upload.js*/
 document.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
     const selectedOption = params.get('option');
     document.getElementById('selected-option').innerText = selectedOption;
 
     const fileInput = document.getElementById("file-input");
-    const uploadBtn = document.getElementById("upload-btn");
+    const browseBtn = document.getElementById("browse-btn");
 
-    // Handle upload button click
-    uploadBtn.addEventListener("click", function() {
+ 
+    browseBtn.addEventListener("click", function() {
         fileInput.click();
     });
 
-    // Handle file input change
+ 
     fileInput.addEventListener("change", function() {
-        // Display selected file name
-        document.getElementById('selected-file').innerText = this.files[0].name;
+       document.getElementById('selected-file').innerText = this.files[0].name;
     });
 });
